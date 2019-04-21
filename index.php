c72a8614a566c3e92f2df8de9f54c07b10fc8c21
YuiFujiwara <48627138+YuiFujiwara@users.noreply.github.com>
2019-04-21 15:41:55 +0900

    Update index.php
    #7

--- index.php
+++ index.php
@@ -1,55 +1,20 @@
 <?php
 
-/*
-$nums[][]
-B:$nums[0] 1-15
-I:$nums[1] 16-30
-..
-O:$nums[4]
+namespace MyApp;
 
-$nums[$i] $i * 15 + 1 〜 $i * 15 + 15
-*/
+class Bingo {
 
-$nums = [];
+  public function create() {
+    $nums = [];
 
-for ($i = 0; $i < 5; $i++) {
-  $col = range($i * 15 + 1, $i * 15 + 15);
-  shuffle($col);
-  $nums[$i] = array_slice($col, 0, 5);
+    for ($i = 0; $i < 5; $i++) {
+      $col = range($i * 15 + 1, $i * 15 + 15);
+      shuffle($col);
+      $nums[$i] = array_slice($col, 0, 5);
+    }
+
+    $nums[2][2] = "FREE";
+    return $nums;
+  }
+
 }
-
-$nums[2][2] = "FREE" ;
-
-// var_dump($nums);
-// exit;
-
-function h($s) {
-  return htmlspecialchars($s, ENT_QUOTES, 'UTF-8');
-}
-
-?>
-
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-  <meta charset="utf-8">
-  <title>BINGO!</title>
-  <link rel="stylesheet" href="styles.css">
-</head>
-<body>
-  <div id="container">
-    <table>
-      <tr>
-        <th>B</th><th>I</th><th>N</th><th>G</th><th>O</th>
-      </tr>
-    <?php for ($i = 0; $i < 5; $i++) : ?>
-    <tr>
-      <?php for ($j = 0; $j < 5; $j++) : ?>
-       <td><?= h($nums[$j][$i]); ?></td>
-     <?php endfor; ?>
-    </tr>
-  <?php endfor; ?>
-    </table>
-  </div>
-</body>
-</html>
